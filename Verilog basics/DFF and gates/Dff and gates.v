38e20b64c68ba53e5793c55ad9aea7d070d6b80c
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-11 13:09:05 +0200

    Implement D flip-flop and logic gates in Verilog

--- Verilog basics/DFF and gates/Dff and gates.v
+++ Verilog basics/DFF and gates/Dff and gates.v
@@ -1,1 +1,16 @@
+module top_module (
+    input clk,
+    input x,
+    output z
+); 
+    reg a,b,c;
+    always@(posedge clk) begin
+        
+        a<=x^a;
+        b<=x&(~b);
+        c<=x|(~c);
+    end         
+        assign z=~(a|b|c);
+
+endmodule
 
