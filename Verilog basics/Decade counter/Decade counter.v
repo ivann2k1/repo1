3be5d9ce38c66f8320561d3c5374672646ebad74
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-11 13:14:10 +0200

    Update Decade counter.v

--- Verilog basics/Decade counter/Decade counter.v
+++ Verilog basics/Decade counter/Decade counter.v
@@ -1,1 +1,10 @@
-
+module top_module (
+    input clk,
+    input reset,       
+    output [3:0] q);
+    always@(posedge clk)begin
+        if(reset) q<=0; 
+        else if(q==9) q<=0;
+        else q<=q+1;
+    end
+endmodule
