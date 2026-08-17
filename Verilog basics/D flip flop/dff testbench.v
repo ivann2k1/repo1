7333330acb9136c421f5ccf080600af441ccd534
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-17 18:30:10 +0200

    Add testbench for D flip-flop module

--- Verilog basics/D flip flop/dff testbench.v
+++ Verilog basics/D flip flop/dff testbench.v
@@ -1,1 +1,33 @@
+`timescale 1ns/1ps
 
+module dff_tb;
+
+  reg clk, d;
+  wire q;
+  
+  dff dut (
+    .clk(clk),
+    .d(d),
+    .q(q)
+  );
+ 
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, dff_tb);
+
+    d = 0;
+
+    #10  d = 1;
+    #10 d = 0;
+    #10 d = 1;
+    #10 d = 0;
+
+    #10 $finish;
+  end
+
+endmodule
