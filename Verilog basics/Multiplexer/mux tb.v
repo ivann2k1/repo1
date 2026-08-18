574f4a6bd3339fcb19e540ebf9fa537e6313cfa6
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-18 20:08:33 +0200

    Update mux tb.v

--- Verilog basics/Multiplexer/mux tb.v
+++ Verilog basics/Multiplexer/mux tb.v
@@ -1,1 +1,45 @@
+`timescale 1ns/1ps
 
+module mux2_1_tb;
+
+    reg a, b, sel;
+    wire y;
+  
+    mux2to1 dut (
+        .a(a),
+        .b(b),
+        .sel(sel),
+        .out(y)
+    );
+    initial begin
+     
+        $dumpfile("mux2_1.vcd");
+        $dumpvars(0, mux2_1_tb);
+
+        a = 0; b = 0; sel = 0;
+        #10;
+
+        a = 1; b = 0; sel = 0;
+        #10;
+
+        a = 0; b = 1; sel = 0;
+        #10;
+
+        a = 1; b = 1; sel = 0;
+        #10;
+
+        a = 0; b = 0; sel = 1;
+        #10;
+
+        a = 1; b = 0; sel = 1;
+        #10;
+
+        a = 0; b = 1; sel = 1;
+        #10;
+
+        a = 1; b = 1; sel = 1;
+        #10;
+
+        $finish;
+    end
+endmodule
