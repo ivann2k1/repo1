ff26e11a898f414fbc3d52536a7309a0963482ee
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-18 12:30:06 +0200

    Add testbench for top_module with clock and input

--- Verilog basics/DFF and gates/DFF and gates testbench.v
+++ Verilog basics/DFF and gates/DFF and gates testbench.v
@@ -1,1 +1,28 @@
+`timescale 1ns/1ps
+
+module top_module_tb;
+  reg clk;
+  reg x;
+  wire z;
+  top_module dut(
+    .clk(clk),
+    .x(x),
+    .z(z)
+  );
+  
+  initial begin
+    clk=0;
+    forever #5 clk=~clk;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,top_module_tb);
+    
+    x=0;
+    #7
+    x=1;
+    #7
+    $finish;
+ end
+endmodule
 
