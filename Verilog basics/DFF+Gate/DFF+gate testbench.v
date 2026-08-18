abcee5f75b7142d9a633a57ec8244fb3d93beb89
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-18 20:04:07 +0200

    Create testbench for D flip-flop module
    Added a testbench for the D flip-flop module to simulate its behavior.

--- Verilog basics/DFF+Gate/DFF+gate testbench.v
+++ Verilog basics/DFF+Gate/DFF+gate testbench.v
@@ -1,1 +1,43 @@
-
+`timescale 1ns/1ps
+​
+module top_module_tb;
+​
+  reg clk;
+  reg in;
+  wire out;
+​
+  top_module dut(
+    .clk(clk),
+    .in(in),
+    .out(out)
+  );
+​
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+​
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, top_module_tb);
+​
+    in = 0;
+​
+    #7;
+    in = 1;
+​
+    #10;
+    in = 0;
+​
+    #10;
+    in = 1;
+​
+    #10;
+    in = 0;
+​
+    #5;
+    $finish;
+  end
+​
+endmodule
+​
