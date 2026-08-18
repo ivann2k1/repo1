ac29ffcb2411278f83682705dc4f65fe768d6767
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-18 11:22:24 +0200

    Add testbench for 7420 chip module

--- Verilog basics/7420 Chip/7420 chip testbench.v
+++ Verilog basics/7420 Chip/7420 chip testbench.v
@@ -1,1 +1,37 @@
+module top_module_tb; 
+  reg p1a, p1b, p1c, p1d;
+  wire p1y;
+  reg p2a, p2b, p2c, p2d;
+  wire p2y; 
 
+top_module dut ( 
+  .p1a(p1a), 
+  .p1b(p1b), 
+  .p1c(p1c), 
+  .p1d(p1d), 
+  .p1y(p1y), 
+  .p2a(p2a), 
+  .p2b(p2b), 
+  .p2c(p2c), 
+  .p2d(p2d),
+  .p2y(p2y) );
+  initial begin 
+    $dumpfile("dump.vcd");
+    $dumpvars(0,top_module_tb);
+    p1a=0; p1b=0; p1c=0; p1d=0; p2a=0; p2b=0; p2c=0; p2d=0;
+    #10;
+   
+    p1a=1; p1b=1; p1c=1; p1d=1; p2a=1; p2b=1; p2c=1; p2d=1;
+    #10; 
+    
+    p1a=1; p1b=1; p1c=1; p1d=0; p2a=1; p2b=1; p2c=1; p2d=0;
+    #10; 
+   
+    p1a=1; p1b=0; p1c=1; p1d=1; p2a=0; p2b=1; p2c=1; p2d=1; 
+    #10;
+  
+    p1a=0; p1b=1; p1c=0; p1d=1; p2a=1; p2b=0; p2c=1; p2d=0; 
+    #10; 
+    $finish;
+  end
+endmodule
