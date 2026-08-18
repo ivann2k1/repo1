24718b679b7a23298c058b0844a596833fafa744
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-18 19:46:04 +0200

    Update 7458 testbench.v

--- Verilog basics/7458_Chip/7458 testbench.v
+++ Verilog basics/7458_Chip/7458 testbench.v
@@ -1,1 +1,45 @@
-
+module top_module_tb;
+  
+  reg p1a,p1b,p1c,p1d,p1e,p1f;
+  
+  wire p1y;
+  
+  reg p2a,p2b,p2c,p2d;
+  
+  wire p2y;
+  top_module dut(
+  
+    .p1a(p1a),
+    .p1b(p1b),
+    .p1c(p1c),
+    .p1d(p1d),
+    .p1e(p1e),
+    .p1f(p1f),
+    .p1y(p1y),
+    .p2a(p2a),
+    .p2b(p2b),
+    .p2c(p2c),
+    .p2d(p2d),
+    .p2y(p2y)
+  );
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,top_module_tb);
+    
+    p1a=0; p1b=0; p1c=0; p1d=0; p1e=0; p1f=0; p2a=0; p2b=0; p2c=0; p2d=0;
+    #5;
+   
+    p1a=1; p1b=1; p1c=1; p1d=1; p1e=1; p1f=1; p2a=1; p2b=1; p2c=1; p2d=1;
+    #5; 
+    
+    p1a=1; p1b=1; p1c=1; p1d=0; p1e=1; p1f=1; p2a=1; p2b=1; p2c=1; p2d=0;
+    #5; 
+   
+    p1a=1; p1b=0; p1c=1; p1d=1; p1e=1; p1f=1; p2a=0; p2b=1; p2c=1; p2d=1; 
+    #5;
+  
+    p1a=0; p1b=1; p1c=0; p1d=1; p1e=1; p1f=1; p2a=1; p2b=0; p2c=1; p2d=0; 
+    #5; 
+    $finish;
+  end
+endmodule
