785d44165532ac38dc454334d38111231f999006
Ivan Majstorović <ivanmajstorovic012@gmail.com>
2026-08-22 19:07:30 +0200

    Update add16.v

--- Verilog basics/Module ADD16/add16.v
+++ Verilog basics/Module ADD16/add16.v
@@ -6,7 +6,7 @@ module top_module(
     wire cin,cout;
     wire [15:0] sum1,sum2;
     add16 a1(a[15:0], b[15:0],1'b0,sum1,cout);
-    add16 a2(a[31:16], b[31:16],cout,sum2,);
+    add16 a2(a[31:16], b[31:16],cout,sum2);
     assign sum={sum2,sum1};
 
 endmodule
